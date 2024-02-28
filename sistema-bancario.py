3f30aa0d34194548ed02ce10138db15b7b15297f
matheus4sn <matheus4sn@gmail.com>
2024-02-28 14:05:03 -0300

    fix: Syntax error

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -58,7 +58,7 @@ while True:
   elif opcao == "e":
     print("\n===== EXTRATO =====")
     print("Não foram realizadas movimentações." if not extrato else extrato)
-    print(f"\nSaldo: R$ {saldo:2.f}")
+    print(f"\nSaldo: R$ {saldo:2f}")
     print("===================")
 
   elif opcao == "e":
