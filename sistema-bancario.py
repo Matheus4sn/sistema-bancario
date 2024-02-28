7ea65ca97c39733967490f0920bce4abc9801814
matheus4sn <matheus4sn@gmail.com>
2024-02-28 13:47:29 -0300

    feat: Add the bank deposit feature

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -14,3 +14,17 @@ limite = 500
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
+
+while True:
+
+  opcao = input(menu)
+
+  if opcao == "d":
+    valor = float(input("Informe o valor do depósito: "))
+
+    if valor > 0:
+      saldo += valor
+      extrato += f"Depósito: R$ {valor:.2f}\n"
+    
+    else:
+      print("Operação falhou! O valor informado é inválido.")
